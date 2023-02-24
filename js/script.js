230f5839cf13c5f9a33a07a82126ac99e62638cb
Ryusaem <53605044+Ryusaem@users.noreply.github.com>
2023-02-24 21:01:53 +0100

    v2 - Refraction done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 window.onload = function() {
-
+    
 
     function removeTransition(event){
-        
         if (event.propertyName != 'transform') return; // skip if it's not a transform
         console.log(event.propertyName);
         this.classList.remove('playing');
     }
 
-    window.addEventListener('keydown', function(event){
+
+    function playSound(event) {
         // console.log(event.keyCode);
         const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`); //we are using the *ATTRIBUTE SELECTOR* from CSS, targeting element[attribute="value"] to select all audio element, with the attribute *data-key* with the a number generated from the event.keyCode
             // console.log(audio);
@@ -16,23 +16,16 @@ window.onload = function() {
             // console.log(event)  
 
         audio.currentTime = 0; // it rewind the sound.
-        audio.play()
+        audio.play();
 
         const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
             // console.log(key)
         key.classList.add('playing');
-
-        
-    })
+    }
 
 
     const keys = document.querySelectorAll('.key');
     keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
-    
-    
-
-
-
+    window.addEventListener('keydown', playSound);
 
 }
